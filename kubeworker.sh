7621aedbcd3eaeb0e9b4a5c74d3558dc3eb408d9
pwinning1991 <ftcpjw@yahoo.com>
2019-10-05 16:18:45 -0400

    updating typo

--- kubeworker.sh
+++ kubeworker.sh
@@ -37,8 +37,8 @@ usermod -a -G docker $(whoami)
 
 #set kernel parameters 
 cat <<EOF> /etc/sysctl.d/k8s.conf
-met.bridge.bridge-nf-call-ip6tables = 1
-met.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
 EOF
 
 #reload kernel parameters
